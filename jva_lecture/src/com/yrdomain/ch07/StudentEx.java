a9d1f3999e46398d2162e2eec3a087e366bab20a
김연근 <@DESKTOP-LBNI81N>
2021-01-18 16:07:24 +0900

    test7

---
+++ jva_lecture/src/com/yrdomain/ch07/StudentEx.java
@@ -0,0 +1,13 @@
+package com.yrdomain.ch07;
+
+public class StudentEx {
+
+		public static void main(String[] args) {
+			Student student = new Student("±è´ÙÀº", "1234567-1234567", 1);
+			System.out.println("name: " + student.name);
+			System.out.println("ssn : " + student.ssn);
+			System.out.println("studentNo: " + student.studentNo);
+			
+		}
+
+	}
