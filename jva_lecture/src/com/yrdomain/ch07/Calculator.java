85c594a6f19a892fad353ef38793b32798127a96
김연근 <@DESKTOP-LBNI81N>
2021-01-19 14:45:48 +0900

    test7

---
+++ jva_lecture/src/com/yrdomain/ch07/Calculator.java
@@ -0,0 +1,9 @@
+package com.yrdomain.ch07;
+
+public class Calculator {
+	
+		double areaCircle(double r) {
+			System.out.println("Calculator °´Ã¼ÀÇ areCircle() ½ÇÇà");
+			return 3.1459 * r * r;
+		}
+	}
---
+++ jva_lecture/src/com/yrdomain/ch07/PhoneEx.java
@@ -0,0 +1,13 @@
+package com.yrdomain.ch07;
+
+public class PhoneEx {
+	public static void main(String[] args) {
+		//Phone phone = new Phone();
+		SmartPhone smartPhone = new SmartPhone("ȫ�浿");
+		
+		smartPhone.turnOn();
+		smartPhone.internetSearch();
+		smartPhone.turnOff();
+	}
+
+}
